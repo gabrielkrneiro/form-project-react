1b1edbd8eb7d0da72422c724c4b8657c6be782b0
Gabu <carneiro.development@gmail.com>
2021-03-19 01:11:48 -0400

    Add typography, Container in App.jsx to bind the application and so on

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,26 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Button from "@material-ui/core/Button";
-import { TextField } from "@material-ui/core";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 export default function FormularioCadastro() {
   return (
     <form action="">
-      <TextField id="nome" label="Nome" variant="outlined" fullWidth />
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
       <TextField
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
         fullWidth
+        margin="normal"
       />
 
-      <TextField id="cpf" label="CPF" variant="outlined" fullWidth />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
-      <TextField id="promo" label="Promo" variant="outlined" fullWidth />
+      <FormControlLabel
+        label="Promocoes"
+        control={
+          <Switch name="promocoes" defaultChecked={true} color="primary" />
+        }
+      />
 
-      <label htmlFor="novidades">Novidades</label>
-      <input type="checkbox" name="novidades" id="novidades" />
+      <FormControlLabel
+        label="novidades"
+        control={
+          <Switch name="novidades" defaultChecked={true} color="primary" />
+        }
+      />
 
       <Button variant="contained" color="primary" type="submit">
         Cadastrar
