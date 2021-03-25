438717284d661513353a63be4c12ec02cf938529
Gabu <carneiro.development@gmail.com>
2021-03-24 23:13:05 -0400

    Add attribute handlers

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,53 +1,76 @@
 import { Button, TextField } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 
-const DadosEntrega = () => (
-  <form action="">
-    <TextField
-      id="cep"
-      label="CEP"
-      type="number"
-      variant="outlined"
-      margin="normal"
-    />
+const DadosEntrega = ({ aoEnviar }) => {
+  const [cep, setCep] = useState(0);
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
 
-    <TextField
-      id="endereco"
-      label="Endereco"
-      type="text"
-      variant="outlined"
-      margin="normal"
-      fullWidth
-    />
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ cep, endereco, numero, estado, cidade });
+      }}
+    >
+      <TextField
+        id="cep"
+        label="CEP"
+        type="number"
+        variant="outlined"
+        margin="normal"
+        value={cep}
+        onChange={(event) => setCep(event.target.value)}
+      />
 
-    <TextField
-      id="numero"
-      label="Numero"
-      type="number"
-      variant="outlined"
-      margin="normal"
-    />
+      <TextField
+        id="endereco"
+        label="Endereco"
+        type="text"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        value={endereco}
+        onChange={(event) => setEndereco(event.target.value)}
+      />
 
-    <TextField
-      id="estado"
-      label="Estado"
-      type="text"
-      variant="outlined"
-      margin="normal"
-    />
+      <TextField
+        id="numero"
+        label="Numero"
+        type="number"
+        variant="outlined"
+        margin="normal"
+        value={numero}
+        onChange={(event) => setNumero(event.target.value)}
+      />
 
-    <TextField
-      id="cidade"
-      label="Cidade"
-      type="text"
-      variant="outlined"
-      margin="normal"
-    />
+      <TextField
+        id="estado"
+        label="Estado"
+        type="text"
+        variant="outlined"
+        margin="normal"
+        value={estado}
+        onChange={(event) => setEstado(event.target.value)}
+      />
 
-    <Button type="submit" variant="contained" color="primary" fullWidth>
-      Finalizar cadastro
-    </Button>
-  </form>
-);
+      <TextField
+        id="cidade"
+        label="Cidade"
+        type="text"
+        variant="outlined"
+        margin="normal"
+        value={cidade}
+        onChange={(event) => setCidade(event.target.value)}
+      />
+
+      <Button type="submit" variant="contained" color="primary" fullWidth>
+        Finalizar cadastro
+      </Button>
+    </form>
+  );
+};
 
 export default DadosEntrega;
--- src/components/FormularioCadastro/DadosUsuarios.jsx
+++ src/components/FormularioCadastro/DadosUsuarios.jsx
@@ -1,12 +1,15 @@
 import { Button, TextField } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 
 export default function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        aoEnviar({});
+        aoEnviar({ email, senha });
       }}
     >
       <TextField
@@ -17,7 +20,10 @@ export default function DadosUsuario({ aoEnviar }) {
         margin="normal"
         fullWidth
         required
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
       />
+
       <TextField
         id="senha"
         label="Senha"
@@ -26,6 +32,8 @@ export default function DadosUsuario({ aoEnviar }) {
         margin="normal"
         fullWidth
         required
+        value={senha}
+        onChange={(event) => setSenha(event.target.value)}
       />
       <Button type="submit" variant="contained" color="primary">
         Cadastrar
